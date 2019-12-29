20e36c5f93f22403beb0ac33eb271843c725aa86
sin3point14 <manaschaudhary2000@gmail.com>
2019-12-29 23:58:01 +0530

    add addCandidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,19 @@
 pragma solidity ^0.5.11;
 
 contract Election{
-    string public candidate;
-    constructor() public {
-        candidate = "LOL";
+    uint public cCount;
+    struct Candidate {
+        uint id;
+        string name;
+        uint votes;
+    }
+    mapping(uint => Candidate) public candidates;
+    constructor () public {
+        addCandidate("ChadRagi");
+        addCandidate("NoobRagi");
+    }
+    function addCandidate(string memory _name) private {
+        cCount++;
+        candidates[cCount] = Candidate(cCount, _name, 0);
     }
 }
